1c3184fd8ce287ebb774be9cd476e133d8acdaf5
CasLinden <cas_linden@msn.com>
2022-04-26 14:01:08 +0900

    finished the header

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+// function scaleAvatar(x) {
+//     const avatar = document.querySelector(`.avatar${x}`);
+//     console.log(avatar)
+//     let avatarHeight = avatar.clientHeight;
+//     console.log(avatarHeight)
+//     avatar.getElementsByClassName.width = "avatarHeight"
+
+
+    
+// }
+// scaleAvatar("1")
